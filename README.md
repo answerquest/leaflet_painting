ff19114b7f8d20618519533103f3ff70cb7a097a
answerquest <nikhil.js@gmail.com>
2022-05-19 12:04:18 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Leaflet Painting Demo
+
+POC using leaflet js to render large paintings and other large imagery in pan-and-zoom slippy map tiles form so that browsers can load them fast and with minimal bandwidth of the server consumed.
+
+Live on 
+
+Tiles are hosted at https://server.nikhilvj.co.in/tilemaker/
+
+
+## Run on local
+Just double-clicking the .html should do the job.
+
+
---
+++ config.js
@@ -0,0 +1,8 @@
+tilePath = 'babel';
+
+maxZoom = 7;
+
+// attributionHTML = 'Wikimedia Commons: <a href="https://commons.wikimedia.org/wiki/Template:Radha_and_Krishna_in_the_boat_of_love_-_Google_Art_Project">Radha and Krishna in the boat of love</a>, National Museum, New Delhi, India';
+attributionHTML = 'most images are from wikimedia commons. Detailed attributions coming soon.';
+
+extension = '.jpg';
